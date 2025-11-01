4f3e8a904c4431d66da339bafcf0783502524930
pavankumar1909 <pavankumarangajala09@gmai.com>
2025-11-01 16:38:57 +0530

    removed log install build

--- build/my_robot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pavan/pavan/ugv_ws/src/my_robot_bringup
-# Build directory: /home/pavan/pavan/ugv_ws/build/my_robot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
